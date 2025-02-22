a8e431674bde9e674f141862e51a97d0b920374e
pastorcito6 <gael.cortez267749@potros.itson.edu.mx>
2025-02-22 16:01:04 -0700

    se agrego nuevos atributos

--- src/conversionmoneda/Moneda.java
+++ src/conversionmoneda/Moneda.java
@@ -1,4 +1,16 @@
 package conversionmoneda;
 
 public enum Moneda {
+    DO(20.44), EU(21.36), MX(20);
+
+    private double valor;
+
+    Moneda(double valor) {
+        this.valor = valor;
+
+    }
+    public double getvalor() {
+    return valor;
+
+    }
 }
