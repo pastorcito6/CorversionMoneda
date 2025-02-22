d1d7a894b37e0b3f0cfec776b63c77eae7aa13ef
pastorcito6 <gael.cortez267749@potros.itson.edu.mx>
2025-02-22 16:29:52 -0700

    se agrego constructor y getter and setter

--- src/conversionmoneda/Cliente.java
+++ src/conversionmoneda/Cliente.java
@@ -1,6 +1,28 @@
 package conversionmoneda;
 
 public class Cliente {
-    double cantidad;
+    private double cantidad;
+    private Moneda tipoMoneda;
 
+    public Cliente(double cantidad, Moneda tipoMoneda) {
+        this.cantidad = cantidad;
+        this.tipoMoneda = tipoMoneda;
+    }
+
+    public Moneda getTipoMoneda() {
+        return tipoMoneda;
+    }
+
+    public void setTipoMoneda(Moneda tipoMoneda) {
+        this.tipoMoneda = tipoMoneda;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+    
 }
