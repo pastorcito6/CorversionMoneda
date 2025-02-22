48dfcb8e654958ff086b7b9437f2019ba410b896
Tyler Brock <pedrohardbass@gmail.com>
2025-02-22 16:04:06 -0700

    uwu

--- src/conversionmoneda/OperacionConversion.java
+++ src/conversionmoneda/OperacionConversion.java
@@ -3,11 +3,22 @@ package conversionmoneda;
 public enum OperacionConversion {
     CANTIDAD {
         @Override
-        public double calcular(double a, double b) {
-            return 0;
+        public double calcular(double moneda, double dinero) {
+            double cantidad = OperacionAritmetica.DIVIDIR.calcular(moneda, dinero);
+
+            return cantidad;
         }
 
-    };
+    },
+    //falta
+    TOTAL{
+        @Override
+        public double calcular(double moneda, double cantidad) {
+            double total = OperacionAritmetica.MULTIPLICAR.calcular(moneda, cantidad);
+            return total;
+        }
+    }
+    ;
     public abstract double calcular(double a, double b);
 
     }
