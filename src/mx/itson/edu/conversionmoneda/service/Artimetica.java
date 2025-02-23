818c015678d09a83502ee423d971a15ae8c8b9be
Tyler Brock <pedrohardbass@gmail.com>
2025-02-22 22:27:17 -0700

    v.3 se hizo optimizacion

--- src/mx/itson/edu/conversionmoneda/service/Artimetica.java
+++ src/mx/itson/edu/conversionmoneda/service/Artimetica.java
@@ -10,26 +10,19 @@ public enum Artimetica {
     RESTAR {
         @Override
         public double calcular(double total, double dinero) {
-            double cambio = total - dinero; //120 - 130 = 10
-
-            return cambio;//10
+            return total - dinero;
         }
     },
     MULTIPLICAR {
         @Override
         public double calcular(double moneda, double cantidad) {
-            double total = moneda * cantidad ;//20 * 6 = 120
-            //obtener total de precio de moneda
-            return total;//120
+            return moneda * cantidad;//120
         }
     },
     DIVIDIR {
         @Override
         public double calcular(double moneda, double dinero) {
-            //operacion para obtener la cantidad que me alcanza
-            double cantidad = moneda / dinero; //
-//          //obtiene la cantidad de moneda que me alcanza
-            return cantidad;
+            return moneda / dinero;
         }
     }
     ;
